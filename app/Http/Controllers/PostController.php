c5b71851da631c77affc61c1dd4eceea9362c9e8
dika <wold.raqi@gmail.com>
2025-06-19 19:50:28 +0700

    improvement api resource

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
     public function index()
     {
         $posts = Post::all();
-        return response()->json(['data'=>$posts]);
+        // return response()->json(['data'=>$posts]);
+        return PostResource::collection($posts);
+    }
+
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+        return new PostResource($post);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
 
 
