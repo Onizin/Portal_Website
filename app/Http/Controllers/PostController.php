a301a60e7acc4ae4d8d2b3a7dd96d762502b3711
dika <wold.raqi@gmail.com>
2025-06-21 20:53:36 +0700

    redirect api

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -27,4 +27,24 @@ class PostController extends Controller
         $post = Post::findOrFail($id);
         return new PostDetailResource($post);
     }
+
+    public function store(Request $request)
+    {
+        // $request->validate([
+        //     'title' => 'required|string|max:255',
+        //     'news_content' => 'required|string',
+        //     'author' => 'required|string|max:100',
+        //     'created_at' => 'nullable|date',
+        // ]);
+        // $post = Post::create([
+        //     'title' => $request->title,
+        //     'news_content' => $request->news_content,
+        //     'author' => $request->author,
+        //     'created_at' => $request->created_at ? date($request->created_at) : now(),
+        // ]);
+        // return response()->json(['data' => new PostDetailResource($post)], 201);
+        return response()->json([
+            'message' => 'This endpoint is not implemented yet.'
+        ], 501);
+    }
 }
